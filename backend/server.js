dd62d25304c89865b9e10d9a0267d9c1bf5f4100
Emanuelmsjv <melwinjoel2008@gmail.cam>
2026-06-26 20:11:02 +0530

    Add volkary-melwin domain to CORS

--- backend/server.js
+++ backend/server.js
@@ -7,7 +7,7 @@ const app = express()
 
 // Configure CORS to accept requests from Vercel domain
 app.use(cors({
-  origin: ['http://localhost:5173', 'https://volkary.vercel.app'],
+  origin: ['http://localhost:5173', 'https://volkary.vercel.app', 'https://volkary-melwin.vercel.app'],
   credentials: true
 }))
 
