e1a90c98d646d0f88c17921d3cbd209c344a4f38
Emanuelmsjv <melwinjoel2008@gmail.cam>
2026-06-26 16:34:26 +0530

    Use dynamic PORT for deployment

--- backend/server.js
+++ backend/server.js
@@ -38,7 +38,8 @@ async function start() {
         res.json(result)
     })
 
-    app.listen(3000, () => console.log("Running on 3000"))
+    const PORT = process.env.PORT || 3000
+    app.listen(PORT, () => console.log(`Running on ${PORT}`))
 }
 
 start()
